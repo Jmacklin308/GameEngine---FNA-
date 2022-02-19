d22cf166d5b0dfd9bd9e83e0367a43fbb8481a2d
Justin Macklin <jmacklin308@gmail.com>
2022-02-19 01:16:46 -0600

    starter stuff

---
+++ GameEngine/Game1.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameEngine
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public Game1() //This is the constructor, this function is called whenever the game class is created.
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+
+        protected override void Update(GameTime gameTime)
+        {
+            //Update the things FNA handles for us underneath the hood:
+            base.Update(gameTime);
+        }
+
+
+        protected override void Draw(GameTime gameTime)
+        {
+            //This will clear what's on the screen each frame, if we don't clear the screen will look like a mess:
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            //Draw the things FNA handles for us underneath the hood:
+            base.Draw(gameTime);
+        }
+    }
+}
